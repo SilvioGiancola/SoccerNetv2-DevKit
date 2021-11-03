fb078911573409f48b85ee854dc8f09f432ee3a6
Anthony Cioppa <cioppa.anthony@gmail.com>
2021-11-03 15:53:38 +0100

    Context Aware Loss in a cleaner format

--- Task1-ActionSpotting/CALF/src/loss.py
+++ Task1-ActionSpotting/CALF/src/loss.py
@@ -19,15 +19,40 @@ class ContextAwareLoss(torch.nn.Module):
 
     def forward(self, gt_label, pred_score):
 
-        params = self.K
+        K = self.K
         hit_radius = self.hit_radius
         miss_radius = self.miss_radius
-        y_true = gt_label
-        y_pred = pred_score
+
+        zeros = torch.zeros(pred_score.size()).to(pred_score.device).type(torch.float)
+        pred_score = 1.-pred_score
         
-        loss = torch.sum(-torch.min(0.*y_true,torch.sign(y_true))*torch.max(0.*y_pred,-torch.log(y_pred+(1.-y_pred)*(torch.min(torch.max(y_true,params[0,:]),params[1,:])-params[0,:])/(params[1,:]-params[0,:]))+torch.log(torch.tensor(miss_radius)))+(1.-torch.max(0.*y_true,torch.sign(-y_true)))*torch.max(0.*y_pred,torch.max(-torch.log(1.-y_pred+torch.abs(y_true)/params[2,:])+torch.log(1.-torch.tensor(hit_radius)),-torch.log(y_pred+(torch.min(y_true,params[3,:])-params[3,:])/(params[2,:]-params[3,:]))+torch.log(torch.tensor(miss_radius)))))
+        case1 = self.DownStep(gt_label, K[0]) * torch.max(zeros, - torch.log(pred_score) + torch.log(zeros + miss_radius))
+        case2 = self.Interval(gt_label, K[0], K[1]) * torch.max(zeros, - torch.log(pred_score + (1.-pred_score)*(self.PartialIdentity(gt_label,K[0],K[1])-K[0])/(K[1]-K[0])) + torch.log(zeros + miss_radius))
+        case3 = self.Interval(gt_label, K[1], 0.) * zeros
+        case4 = self.Interval(gt_label, 0., K[2]) * torch.max(zeros, - torch.log(1.-pred_score + pred_score*(self.PartialIdentity(gt_label,0.,K[2])-0.)/(K[2]-0.)) + torch.log(zeros + 1.-hit_radius))
+        case5 = self.Interval(gt_label, K[2], K[3]) * torch.max(zeros, - torch.log(pred_score + (1.-pred_score)*(self.PartialIdentity(gt_label,K[2],K[3])-K[3])/(K[2]-K[3])) + torch.log(zeros + miss_radius))
+        case6 = self.UpStep(gt_label, K[3]) * torch.max(zeros, - torch.log(pred_score) + torch.log(zeros + miss_radius))
         
-        return loss
+        
+        L = case1 + case2 + case3 + case4 + case5 + case6
+        
+        return torch.sum(L)
+
+    def UpStep(self,x,a): #0 if x<a, 1 if x >= a
+
+        return 1.-torch.max(0.*x,torch.sign(a-x))
+
+    def DownStep(self,x,a): #1 if x < a, 0 if x >=a
+
+        return torch.max(0.*x,torch.sign(a-x))
+
+    def Interval(self,x,a,b): # 1 if a<= x < b, 0 otherwise
+        
+        return self.UpStep(x,a) * self.DownStep(x,b)
+
+    def PartialIdentity(self,x,a,b):#a if x<a, x if a<= x <b, b if x >= b
+
+        return torch.min(torch.max(x,0.*x+a),0.*x+b)
 
 ####################################################################################################################################################
 
